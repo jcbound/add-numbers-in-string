8d54f2f1cab64cb7e05155a8331fd02d7ae37c25
weiranyi <168578203@qq.com>
2020-11-27 16:27:59 +0800

    使用了if嵌套

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -10,11 +10,15 @@ public class Main {
     // 例如，给定a="123",b="456"，返回"579"，因为123+456=579
     // 注意，若参数为null，则当作0处理，即add("123", null)=="123", add(null, null)=="0"
     public static String add(String a, String b) {
-        if (a == null) {
-            a = String.valueOf(0);
-        }
-        if (b == null) {
-            b = String.valueOf(0);
+        if (a == null && b == null) {
+            return String.valueOf(0);
+        }else{
+            if (a == null) {
+                return String.valueOf(0 + Integer.parseInt(b));
+            }
+            if (b == null) {
+                return String.valueOf(Integer.parseInt(a) + 0);
+            }
         }
         return String.valueOf(Integer.parseInt(a) + Integer.parseInt(b));
     }
