dd7a936761e588d0046ee4eaef589bd5c9d102ef
JerryWang <1976883731@qq.com>
2021-10-30 04:04:08 +0800

    Optimized

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -11,8 +11,11 @@ public class Main {
     // 例如，给定a="123",b="456"，返回"579"，因为123+456=579
     // 注意，若参数为null，则当作0处理，即add("123", null)=="123", add(null, null)=="0"
     public static String add(String a, String b) {
-        if (a == null) a = "0";
-        if (b == null) b = "0";
-        return String.valueOf(Integer.parseInt(a) + Integer.parseInt(b));
+        return String.valueOf(parseInt(a) + parseInt(b));
+    }
+
+    private static int parseInt(String s) {
+        if (s == null) return 0;
+        return Integer.parseInt(s);
     }
 }
